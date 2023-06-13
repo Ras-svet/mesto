ca819fd5d7f697652549ed4dfc22a4efbe8c4ce5
Ras-svet <kulagina.cveta@yandex.ru>
2023-06-14 01:58:00 +0300

    fix: исправить комментарии ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -2,31 +2,30 @@ let popup = document.querySelector(".popup");
 let formElement = document.querySelector(".popup__forms");
 let nameInput = formElement.querySelector(".popup__field_type_name");
 let jobInput = formElement.querySelector(".popup__field_type_job");
-let saveButton = formElement.querySelector(".popup__save-button");
 let closeButton = document.querySelector(".popup__close-button");
 let editButton = document.querySelector(".profile__edit-button");
 let personName = document.querySelector(".profile__name");
 let personJob = document.querySelector(".profile__job");
 
+function closePopup() {
+	popup.classList.remove('popup_opened')
+}
+
+function handleFormSubmit (evt) {
+	evt.preventDefault(); 
+	let name = nameInput.value
+	let job = jobInput.value
+
+	personName.textContent = name;
+	personJob.textContent = job;
+	closePopup();
+}
+
 editButton.addEventListener('click', function() {
-	popup.classList.add('popup__opened');
+	popup.classList.add('popup_opened');
 	nameInput.value = personName.textContent;
 	jobInput.value = personJob.textContent;
 })
 
-function closePopup() {
-	popup.classList.remove('popup__opened')
-}
-
 closeButton.addEventListener('click', closePopup);
-
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-		let name = nameInput.value
-		let job = jobInput.value
-
-		personName.textContent = name;
-		personJob.textContent = job;
-}
-saveButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit); 
