5c0dc80114c44273fc0c38b5ca6875e9e5c09f36
Ras-svet <kulagina.cveta@yandex.ru>
2023-06-27 16:21:43 +0300

    fix: исправить замечания ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
+const elementCards = document.querySelector('.elements');
 // переменные для popup профиля
 const popupProfile = document.querySelector(".popup_type_profile");
 const formProfileElement = document.querySelector('[name="user-information"]');
 const nameInput = formProfileElement.querySelector(".popup__field_type_name");
 const jobInput = formProfileElement.querySelector(".popup__field_type_job");
-const closePopupProfile = popupProfile.querySelector(".popup__close-button");
-const editProfile = document.querySelector(".profile__edit-button");
+const buttonClosePopupProfile = popupProfile.querySelector(".popup__close-button");
+const buttonEditProfile = document.querySelector(".profile__edit-button");
 const personName = document.querySelector(".profile__name");
 const personJob = document.querySelector(".profile__job");
 // переменные для poopup добавления карточки
@@ -12,13 +13,13 @@ const popupCard = document.querySelector(".popup_type_card")
 const formCardElement = document.querySelector('[name="card-information"]');
 const titleCardInput = popupCard.querySelector(".popup__field_type_title");
 const srcCardInput = popupCard.querySelector(".popup__field_type_src")
-const closePopupCard = popupCard.querySelector(".popup__close-button");
-const addPopupCard = document.querySelector(".profile__add-button");
+const buttonClosePopupCard = popupCard.querySelector(".popup__close-button");
+const buttonAddPopupCard = document.querySelector(".profile__add-button");
 // переменные для открытия карточки в полноэкранный режим
 const popupFullPicture = document.querySelector(".popup_type_fullscreen");
 const titleFullPicture = popupFullPicture.querySelector(".popup__text");
 const photoFullPicture = popupFullPicture.querySelector(".popup__picture");
-const closeButtonFullPicture = popupFullPicture.querySelector(".popup__close-button");
+const buttonCloseFullPicture = popupFullPicture.querySelector(".popup__close-button");
 // открытие popup
 function openPopup (popup) {
 	popup.classList.add('popup_opened')
@@ -26,8 +27,8 @@ function openPopup (popup) {
 // изменение инфрормации профиля
 function handleFormSubmit (evt) {
 	evt.preventDefault(); 
-	let name = nameInput.value;
-	let job = jobInput.value;
+	const name = nameInput.value;
+	const job = jobInput.value;
 	personName.textContent = name;
 	personJob.textContent = job;
 	closePopup(popupProfile);
@@ -36,14 +37,11 @@ function handleFormSubmit (evt) {
 function createCard (evt) {
 	evt.preventDefault();
 	const newCard = {name: titleCardInput.value, link: srcCardInput.value}
-	console.log(newCard)
-	addCard(newCard)
 	closePopup(popupCard)
-	return addCard
+	return newCard
 }
 // отрисовка карточек
 function addCard(element) {
-	const elementCards = document.querySelector('.elements');
 	const elementTemplate = document.querySelector('#element').content;
 	const elementCard = elementTemplate.querySelector('.element').cloneNode(true);
 	elementCard.querySelector('.element__trash').addEventListener('click', () => {
@@ -51,6 +49,7 @@ function addCard(element) {
 	})
 	elementCard.querySelector('.element__title').textContent = element.name;
 	elementCard.querySelector('.element__image').src = element.link;
+	elementCard.querySelector('.element__image').alt = element.name;
 	elementCard.querySelector('.element__image').addEventListener('click', () => {
 		openPopup(popupFullPicture);
 		createFullscreen(element.link, element.name);
@@ -58,7 +57,8 @@ function addCard(element) {
 	elementCard.querySelector('.element__like').addEventListener('click', (evt) => {
 		evt.target.classList.toggle('element__like_active')
 	})
-	elementCards.append(elementCard);
+	return elementCard;
+	// elementCards.append(elementCard);
 }
 // закрытие popup
 function closePopup (popup) {
@@ -68,22 +68,26 @@ function closePopup (popup) {
 function createFullscreen(link, name) {
 	titleFullPicture.textContent = name;
 	photoFullPicture.src = link;
+	photoFullPicture.alt = name;
 }
 // отрисовка карточек из массива
-initialElements.forEach(addCard)
+initialElements.forEach((item) => elementCards.append(addCard(item)))
 // наполнение popup профиля изначальными значениями
-editProfile.addEventListener('click', function() {
+buttonEditProfile.addEventListener('click', function() {
 	nameInput.value = personName.textContent;
 	jobInput.value = personJob.textContent;
 })
 
-addPopupCard.addEventListener('click', () => openPopup(popupCard))
-closePopupCard.addEventListener('click', () => closePopup(popupCard));
+buttonAddPopupCard.addEventListener('click', () => {
+	openPopup(popupCard);
+	formCardElement.reset();
+})
+buttonClosePopupCard.addEventListener('click', () => closePopup(popupCard));
 
-editProfile.addEventListener('click', () => openPopup(popupProfile));
-closePopupProfile.addEventListener('click', () => closePopup(popupProfile));
+buttonEditProfile.addEventListener('click', () => openPopup(popupProfile));
+buttonClosePopupProfile.addEventListener('click', () => closePopup(popupProfile));
 
 formProfileElement.addEventListener('submit', handleFormSubmit);
-formCardElement.addEventListener('submit', createCard);
+formCardElement.addEventListener('submit', (evt) => elementCards.prepend(addCard(createCard(evt))));
 
-closeButtonFullPicture.addEventListener('click', () => closePopup(popupFullPicture));
+buttonCloseFullPicture.addEventListener('click', () => closePopup(popupFullPicture));
